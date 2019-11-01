d64a7e5f07c54e5d331bc20461585eaa1477c9c8
Hardik Trivedi <hardik@pop-os.localdomain>
2019-11-02 02:38:33 +0530

    add register route to users/urls.py

--- quiz/users/urls.py
+++ quiz/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import quiz_home
+from .views import quiz_home, user_register
 
 urlpatterns = [
     path('', quiz_home, name='quiz-home'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='user-login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
+    path('register/', user_register, name='user-register')
 ]
