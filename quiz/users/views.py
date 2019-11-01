9e8c952ac06e8a2b11a762b4807b3ec26ac3f425
Hardik Trivedi <hardik@pop-os.localdomain>
2019-11-01 22:35:40 +0530

    add initial quiz-home view

--- quiz/users/views.py
+++ quiz/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def quiz_home(request):
+    context = {
+        'user': request.user,
+    }
+    return render(request, 'users/quiz_home.html',  context=context)
