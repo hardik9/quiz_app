8f317ebd1c8ab957868be50607b7e00fa63a6d5a
Hardik Trivedi <hardik@pop-os.localdomain>
2019-11-02 15:48:01 +0530

    fix register form fields for first and last names

--- quiz/users/forms.py
+++ quiz/users/forms.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
-    firstname = forms.CharField(max_length=30)
-    lastname = forms.CharField(max_length=30)
+    # first_name = forms.CharField(max_length=30)
+    # last_name = forms.CharField(max_length=30)
 
     class Meta:
         model = User
-        fields = ['firstname', 'lastname', 'email', 'username', 'password1', 'password2',]
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2',]
