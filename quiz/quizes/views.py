9cd60f38c8f2cedea785ea41741f8f4664af54cb
Hardik Trivedi <hardik9496@gmail.com>
2019-11-05 15:25:10 +0530

    add new_quiz view

--- quiz/quizes/views.py
+++ quiz/quizes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def new_quiz(request):
+    
+    context = {
+        'user': request.user,
+    }
+    return render(request, 'quizes/quiz.html', context=context)
