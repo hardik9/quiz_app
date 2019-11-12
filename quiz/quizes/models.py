05ed70ddf3ce0257306b12f80a0b7f6f281e4dfa
Hardik Trivedi <hardik9496@gmail.com>
2019-11-12 13:29:34 +0530

    add TrueFalseQuestion model

--- quiz/quizes/models.py
+++ quiz/quizes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class TrueFalseQuestion(models.Model):
+
+    choice = [('True', 'True'), ('False', 'False')]
+
+    question = models.CharField(max_length=300)
+    answer = models.CharField(max_length=5, choices=choice)
+
+    def __str__(self):
+        return self.question + ' -- ' + self.answer
+
